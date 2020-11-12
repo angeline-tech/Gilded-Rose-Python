30f6e4813a1c90963b4851602c031f560d433676
Jordan <jordan.angeline1998@gmail.com>
2020-11-12 11:25:19 +0000

    extracted reduceQualityIfNotSulfuras

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -6,9 +6,7 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                self.reduceQualityIfNotSulfuras(item)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -24,11 +22,14 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                        self.reduceQualityIfNotSulfuras(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+
+    def reduceQualityIfNotSulfuras(self, item):
+        if item.quality > 0:
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = item.quality - 1
